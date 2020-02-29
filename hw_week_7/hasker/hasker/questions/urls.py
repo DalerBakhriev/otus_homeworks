97070fb01582254d9c6a378e44c476d2b70bd1a1
DalerBakhriev <DalerBakhriev@mail.ru>
2020-03-01 02:58:44 +0300

    Added general site header, search question by tag view

--- hw_week_7/hasker/hasker/questions/urls.py
+++ hw_week_7/hasker/hasker/questions/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "questions"
 urlpatterns = [
     path("", views.index, name="questions"),
+    path("search_question/", views.search_question, name="search_question"),
     path("ask_question/", views.ask_question, name="ask_question"),
+    path("tag/<int:tag_id>", views.search_question_by_tag, name="search_question_by_tag"),
     path("question/<int:question_id>", views.question_detail, name="question"),
     path("question/<int:question_id>/add_answer", views.add_answer, name="add_answer")
 ]
--- hw_week_7/hasker/hasker/questions/views.py
+++ hw_week_7/hasker/hasker/questions/views.py
@@ -1,16 +1,17 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.http import (
     HttpResponse,
     HttpRequest,
     HttpResponseRedirect,
     Http404
 )
-from django.utils import timezone
-from django.urls import reverse
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+from django.utils import timezone
 
 from .forms import AskQuestionForm
-from .models import Answer, Question
+from .models import Question, Tag
 from .. import settings
 
 
@@ -21,6 +22,30 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request,
                   template_name="questions/questions.html",
                   context={"questions": questions})
+
+
+def search_question(request: HttpRequest) -> HttpResponse:
+
+    query = request.POST["query"]
+    questions_for_query = Question.objects.filter(
+        Q(title__contains=query) | Q(text__contains=query)
+    ).order_by("-rating", "-creation_date")
+
+    return render(request,
+                  template_name="questions/questions.html",
+                  context={"questions": questions_for_query})
+
+
+def search_question_by_tag(request: HttpRequest, tag_id: int) -> HttpResponse:
+
+    try:
+        questions_by_tag = Tag.objects.get(id=tag_id).question_set.all()
+    except ObjectDoesNotExist:
+        raise Http404("Tag not found")
+
+    return render(request,
+                  template_name="questions/questions.html",
+                  context={"questions": questions_by_tag})
 
 
 def ask_question(request: HttpRequest) -> HttpResponse:
@@ -55,7 +80,7 @@ def question_detail(request: HttpRequest, question_id: int) -> HttpResponse:
     if question_id is None:
         return Http404("Could not find question id")
 
-    question = get_question(question_id)
+    question: Question = get_question(question_id)
     answers = question.answers.order_by("-is_correct", "-creation_date").all()
 
     return render(request,
@@ -66,7 +91,7 @@ def question_detail(request: HttpRequest, question_id: int) -> HttpResponse:
 
 def add_answer(request: HttpRequest, question_id: int) -> HttpResponse:
 
-    question = get_question(question_id)
+    question: Question = get_question(question_id)
     question.answers.create(
         text=request.POST["text"],
         author=request.user,
