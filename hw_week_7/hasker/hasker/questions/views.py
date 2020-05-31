405ab3136f8e1faec4b1a5a35c40c59fc5121d55
DalerBakhriev <DalerBakhriev@mail.ru>
2020-06-01 00:33:50 +0300

    rewrote some question views to CBV

--- hw_week_7/hasker/hasker/questions/views.py
+++ hw_week_7/hasker/hasker/questions/views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.views.generic import DetailView, CreateView, ListView
 from django.db.models import Q, F, Count
+from django.db.models.query import QuerySet
 from django.http import (
     HttpResponse,
     HttpRequest,
@@ -13,6 +15,22 @@ from django.utils import timezone
 
 from .forms import AskQuestionForm
 from .models import Question, Tag, User
+
+
+class QuestionListView(ListView):
+
+    template_name = "questions/questions.html"
+    context_object_name = "questions"
+    allow_empty = True
+    model = Question
+    paginate_by = settings.MAX_QUESTIONS_ON_PAGE
+    queryset: QuerySet
+
+    def get_queryset(self):
+        return self.model.objects.annotate(
+            likes=Count("users_who_liked"),
+            dislikes=Count("users_who_disliked"),
+        ).order_by(F("dislikes") - F("likes"), "-creation_date")
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -49,6 +67,25 @@ def search_question_by_tag(request: HttpRequest, tag_id: int) -> HttpResponse:
     return render(request,
                   template_name="questions/questions.html",
                   context={"questions": questions_by_tag})
+
+
+class AskQuestionView(CreateView):
+
+    object: Question
+    form_class = AskQuestionForm
+    model = Question
+    template_name = "questions/ask_question.html"
+
+    def form_valid(self, form: AskQuestionForm) -> HttpResponse:
+
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self) -> str:
+        return self.object.get_absolute_url()
 
 
 def ask_question(request: HttpRequest) -> HttpResponse:
@@ -89,6 +126,29 @@ def dislike_question(request: HttpRequest, question_id: int) -> HttpResponse:
     question.dislike(user=request.user)
 
     return HttpResponseRedirect(reverse("questions:questions"))
+
+
+class QuestionDetailView(DetailView):
+
+    model = Question
+    template_name = "questions/question_detail.html"
+    pk_url_kwarg = "question_id"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        answers = self.object.answers.order_by("-is_correct", "-creation_date").all()
+
+        context["answers"] = answers
+
+        return context
+
+    def get_object(self, queryset=None) -> Question:
+
+        question_id = self.kwargs.get(self.pk_url_kwarg)
+        question_object = get_object_or_404(Question, id=question_id)
+
+        return question_object
 
 
 def question_detail(request: HttpRequest, question_id: int) -> HttpResponse:
