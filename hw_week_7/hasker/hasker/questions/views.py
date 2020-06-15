1906b5fc3a2d1da72e70a98f74ff8f3e929fd90d
DalerBakhriev <DalerBakhriev@mail.ru>
2020-06-15 15:33:52 +0300

    added add answer view as CBV, removed old FBV versions of views

--- hw_week_7/hasker/hasker/questions/views.py
+++ hw_week_7/hasker/hasker/questions/views.py
@@ -6,15 +6,14 @@ from django.db.models.query import QuerySet
 from django.http import (
     HttpResponse,
     HttpRequest,
-    HttpResponseRedirect,
-    Http404
+    HttpResponseRedirect
 )
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
 
 from .forms import AskQuestionForm
-from .models import Question, Tag, User
+from .models import Question, Tag, User, Answer
 
 
 class BaseQuestionListView(ListView):
@@ -39,18 +38,6 @@ class QuestionListView(BaseQuestionListView):
         return questions
 
 
-def index(request: HttpRequest) -> HttpResponse:
-
-    questions = Question.objects.annotate(
-        likes=Count("users_who_liked"),
-        dislikes=Count("users_who_disliked"),
-    ).order_by(F("dislikes") - F("likes"), "-creation_date")[:settings.MAX_QUESTIONS_ON_PAGE]
-
-    return render(request,
-                  template_name="questions/questions.html",
-                  context={"questions": questions})
-
-
 class SearchQuestionListView(BaseQuestionListView):
 
     def get_queryset(self):
@@ -70,36 +57,14 @@ class SearchQuestionListView(BaseQuestionListView):
         return questions_for_query
 
 
-def search_question(request: HttpRequest) -> HttpResponse:
-
-    query = request.POST["query"]
-    questions_for_query = Question.objects.filter(
-        Q(title__icontains=query) | Q(text__icontains=query)
-    ).annotate(
-        likes=Count("users_who_liked"),
-        dislikes=Count("users_who_disliked"),
-    ).order_by(F("dislikes") - F("likes"), "-creation_date")[:settings.MAX_QUESTIONS_ON_PAGE]
-
-    return render(request,
-                  template_name="questions/questions.html",
-                  context={"questions": questions_for_query})
-
-
 class SearchQuestionByTagListView(BaseQuestionListView):
 
     def get_queryset(self):
+
         tag_id = self.kwargs.get("tag_id")
         questions_by_tag = get_object_or_404(Tag, id=tag_id).question_set.all()
+
         return questions_by_tag
-
-
-def search_question_by_tag(request: HttpRequest, tag_id: int) -> HttpResponse:
-
-    questions_by_tag = get_object_or_404(Tag, id=tag_id).question_set.all()
-
-    return render(request,
-                  template_name="questions/questions.html",
-                  context={"questions": questions_by_tag})
 
 
 class AskQuestionView(CreateView):
@@ -121,33 +86,9 @@ class AskQuestionView(CreateView):
         return self.object.get_absolute_url()
 
 
-def ask_question(request: HttpRequest) -> HttpResponse:
-
-    if request.method == 'POST':
-        form = AskQuestionForm(request.POST)
-        if not form.is_valid():
-            raise Http404(f"{form.errors}")
-        question_model = form.save(commit=False)
-        author = request.user
-        question_model.author = author
-        question_model.save()
-        return HttpResponseRedirect(question_model.url)
-
-    form = AskQuestionForm()
-
-    return render(request, "questions/ask_question.html", {'form': form})
-
-
-def get_question(question_id: int) -> Question:
-
-    question = get_object_or_404(Question, id=question_id)
-
-    return question
-
-
 def like_question(request: HttpRequest, question_id: int) -> HttpResponse:
 
-    question: Question = get_question(question_id)
+    question: Question = get_object_or_404(Question, id=question_id)
     question.like(user=request.user)
 
     return HttpResponseRedirect(reverse("questions:questions"))
@@ -155,7 +96,7 @@ def like_question(request: HttpRequest, question_id: int) -> HttpResponse:
 
 def dislike_question(request: HttpRequest, question_id: int) -> HttpResponse:
 
-    question: Question = get_question(question_id)
+    question: Question = get_object_or_404(Question, id=question_id)
     question.dislike(user=request.user)
 
     return HttpResponseRedirect(reverse("questions:questions"))
@@ -184,20 +125,6 @@ class QuestionDetailView(DetailView):
         return question_object
 
 
-def question_detail(request: HttpRequest, question_id: int) -> HttpResponse:
-
-    if question_id is None:
-        return Http404("Could not find question id")
-
-    question: Question = get_question(question_id)
-    answers = question.answers.order_by("-is_correct", "-creation_date").all()
-
-    return render(request,
-                  template_name="questions/question_detail.html",
-                  context={"question": question,
-                           "answers": answers})
-
-
 def notify_question_author(user: User, question: Question) -> None:
 
     send_mail(
@@ -210,18 +137,28 @@ def notify_question_author(user: User, question: Question) -> None:
     )
 
 
-def add_answer(request: HttpRequest, question_id: int) -> HttpResponse:
+class AddAnswerView(CreateView):
 
-    question: Question = get_question(question_id)
-    question.answers.create(
-        text=request.POST["text"],
-        author=request.user,
-        creation_date=timezone.now()
-    )
+    model = Answer
+    pk_url_kwarg = "question_id"
+    http_method_names = ['post']
 
-    notify_question_author(
-        user=request.user,
-        question=question
-    )
+    def form_valid(self, form) -> HttpResponse:
 
-    return HttpResponseRedirect(reverse("questions:question", args=(question.id,)))
+        question_id = self.kwargs.get(self.pk_url_kwarg)
+        question = get_object_or_404(Question, id=question_id)
+
+        answer_text = self.kwargs.get("text")
+        question.answers.create(
+            text=answer_text,
+            author=self.request.user,
+            creation_date=timezone.now()
+        )
+
+        notify_question_author(
+            user=self.request.user,
+            question=question
+        )
+
+        return HttpResponseRedirect(reverse("questions:question", args=(question.id,)))
+
