bc4acf293ac7e2ead9bfa14027feaf2376c32174
DalerBakhriev <DalerBakhriev@mail.ru>
2020-07-25 19:12:14 +0300

    removed default avatar image, added method for getting user image or default image

--- hw_week_7/hasker/hasker/users/forms.py
+++ hw_week_7/hasker/hasker/users/forms.py
@@ -25,9 +25,7 @@ class UserSignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("username", "email", "password", "avatar")
-        labels = {
-            'username': 'login'
-        }
+        labels = {'username': 'login'}
         widgets = {"password": forms.PasswordInput}
 
     field_order = ["username", "email", "password", "repeat_password", "avatar"]
--- hw_week_7/hasker/hasker/users/models.py
+++ hw_week_7/hasker/hasker/users/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime, timedelta
+import os
 
-import jwt
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -9,8 +8,7 @@ from django.db import models
 class User(AbstractUser):
 
     avatar = models.ImageField(
-        default="imgs/default_avatar.jpg",
-        upload_to="imgs/",
+        upload_to="avatars",
         null=True,
         blank=True
     )
@@ -18,24 +16,9 @@ class User(AbstractUser):
     def __str__(self) -> str:
         return self.username
 
-    @property
-    def token(self):
-        return self._generate_jwt_token()
+    def get_avatar_url(self):
+        default_avatar_url = os.path.join(settings.STATIC_URL, "img/default_avatar.jpg")
+        return self.avatar.url if self.avatar else default_avatar_url
 
-    def _generate_jwt_token(self):
-
-        """
-        Generates a JSON Web Token that stores this user's ID and has an expiry
-        date set to 60 days into the future.
-        """
-
-        dt = datetime.now() + timedelta(days=60)
-
-        token = jwt.encode({
-            'id': self.pk,
-            'exp': int(dt.strftime('%s'))
-        }, settings.SECRET_KEY, algorithm='HS256')
-
-        return token.decode('utf-8')
 
 
